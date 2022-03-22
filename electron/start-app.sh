d47949b7aed40b1e219fcb1ddca62b0ff5de882a
Steffen Deusch <steffen@deusch.me>
2022-03-22 13:26:50 +0100

    add SWAYSOCK to electron app env vars

--- electron/start-app.sh
+++ electron/start-app.sh
@@ -3,6 +3,7 @@
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 export XDG_RUNTIME_DIR=/tmp/.wayland
 export WAYLAND_DISPLAY=$(find $XDG_RUNTIME_DIR/ -name "wayland-*" | head -n 1 | cut -d "/" -f 4)
+export SWAYSOCK=$(find $XDG_RUNTIME_DIR/ -name "sway-ipc.*" | head -n 1)
 # uncomment to use xwayland
 # export DISPLAY=:0
 # export XAUTHORITY=/tmp/displayserver/.Xauthority
